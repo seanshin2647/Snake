4f70459ca3a6ee3b71c850b07a086683fc6c05f9
Sean Shin <seanshin2647@gmail.com>
2019-03-31 12:00:36 -0600

    I began work on the __init__ of the player's head.

---
+++ game_classes.py
@@ -0,0 +1,22 @@
+# A file for all of the classes I will be using.
+
+from import_libraries import *
+
+# I plan on having two classes make up the human player,
+# one for the head and one for the snake sections.
+class Player_Head(pygame.sprite.Sprite):
+    def __init__(self, board_width, board_height):
+        super().__init__()
+
+        self.side_length = 20
+
+        self.image - pygame.Surface([self.side_length, self.side_length])
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = random.randrange((0 + (self.side_length * 3)),
+            (board_width - (self.side_length * 3)))
+
+        self.rect.y = random.randrange((0 + (self.side_length * 4)), 
+            (board_height - (self.side_length * 4)))
+            
