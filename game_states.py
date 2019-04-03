0c5cdc2747a7e50efce7eef7c93a0cefddd694e7
Sean Shin <seanshin2647@gmail.com>
2019-04-02 19:40:13 -0600

    I have created the state class to be the parent.

---
+++ game_states.py
@@ -0,0 +1,15 @@
+from game_classes.py import *
+
+class State():
+    def __init__(self):
+        pass
+
+    def render(self):
+        raise NotImplementedError
+
+    def update(self):
+        raise NotImplementedError
+
+    def handle_events(self):
+        raise NotImplementedError
+
